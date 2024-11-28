f2cfed99df16e72826193d7f9650554f102878e3
haqueaminul10 <haqueaminul10100@gmail.com>
2024-11-28 23:48:56 +0600

    sidebar setup ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
+import Navbar from './Components/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import New from './Components/new';
+import Home from './Components/home';
 
 function App() {
-  return <>home</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/new' element={<New />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/header/index.jsx
@@ -0,0 +1,10 @@
+import '../../Components/styles/header/header.css';
+const Navbar = () => {
+  return (
+    <>
+      <div>navbar</div>
+    </>
+  );
+};
+
+export default Navbar;
